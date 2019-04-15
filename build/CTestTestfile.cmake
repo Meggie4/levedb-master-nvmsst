6a585f85fdddc2a62c75bf6afa308c4337aaf3fa
Meggie <1224642332@qq.com>
2019-04-15 23:09:42 +0800

    仍然存在错误，需要修改

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,35 @@
+# CMake generated Testfile for 
+# Source directory: /home/meggie/文档/leveldb-master-nvmsst
+# Build directory: /home/meggie/文档/leveldb-master-nvmsst/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(c_test "/home/meggie/文档/leveldb-master-nvmsst/build/c_test")
+add_test(fault_injection_test "/home/meggie/文档/leveldb-master-nvmsst/build/fault_injection_test")
+add_test(issue178_test "/home/meggie/文档/leveldb-master-nvmsst/build/issue178_test")
+add_test(issue200_test "/home/meggie/文档/leveldb-master-nvmsst/build/issue200_test")
+add_test(env_test "/home/meggie/文档/leveldb-master-nvmsst/build/env_test")
+add_test(status_test "/home/meggie/文档/leveldb-master-nvmsst/build/status_test")
+add_test(no_destructor_test "/home/meggie/文档/leveldb-master-nvmsst/build/no_destructor_test")
+add_test(autocompact_test "/home/meggie/文档/leveldb-master-nvmsst/build/autocompact_test")
+add_test(corruption_test "/home/meggie/文档/leveldb-master-nvmsst/build/corruption_test")
+add_test(db_test "/home/meggie/文档/leveldb-master-nvmsst/build/db_test")
+add_test(dbformat_test "/home/meggie/文档/leveldb-master-nvmsst/build/dbformat_test")
+add_test(filename_test "/home/meggie/文档/leveldb-master-nvmsst/build/filename_test")
+add_test(log_test "/home/meggie/文档/leveldb-master-nvmsst/build/log_test")
+add_test(recovery_test "/home/meggie/文档/leveldb-master-nvmsst/build/recovery_test")
+add_test(skiplist_test "/home/meggie/文档/leveldb-master-nvmsst/build/skiplist_test")
+add_test(version_edit_test "/home/meggie/文档/leveldb-master-nvmsst/build/version_edit_test")
+add_test(version_set_test "/home/meggie/文档/leveldb-master-nvmsst/build/version_set_test")
+add_test(write_batch_test "/home/meggie/文档/leveldb-master-nvmsst/build/write_batch_test")
+add_test(memenv_test "/home/meggie/文档/leveldb-master-nvmsst/build/memenv_test")
+add_test(filter_block_test "/home/meggie/文档/leveldb-master-nvmsst/build/filter_block_test")
+add_test(table_test "/home/meggie/文档/leveldb-master-nvmsst/build/table_test")
+add_test(arena_test "/home/meggie/文档/leveldb-master-nvmsst/build/arena_test")
+add_test(bloom_test "/home/meggie/文档/leveldb-master-nvmsst/build/bloom_test")
+add_test(cache_test "/home/meggie/文档/leveldb-master-nvmsst/build/cache_test")
+add_test(coding_test "/home/meggie/文档/leveldb-master-nvmsst/build/coding_test")
+add_test(crc32c_test "/home/meggie/文档/leveldb-master-nvmsst/build/crc32c_test")
+add_test(hash_test "/home/meggie/文档/leveldb-master-nvmsst/build/hash_test")
+add_test(logging_test "/home/meggie/文档/leveldb-master-nvmsst/build/logging_test")
+add_test(env_posix_test "/home/meggie/文档/leveldb-master-nvmsst/build/env_posix_test")
